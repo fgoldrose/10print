68a08a346d0a1f5502d54f7f9dc0a4a4f4bffdb3
Fred Goldstein-Rose <goldsteinrose@cs.uchicago.edu>
2020-05-05 20:13:49 -0500

    modified

--- 10print.js
+++ 10print.js
@@ -3,18 +3,44 @@
 */
 
 const w = process.stdout.columns
+let offset = Math.random() * w
+let length = Math.random() * (w - offset)
 
 function draw () {
-  setTimeout(draw, 1000)
-  let output = ''
-  for (let i = 0; i < w; i++) {
-    if (Math.random() > 0.5) {
-      output += '\\'
-    } else {
-      output += '/'
+  setTimeout(draw, 100)
+  let output = ' '.repeat(offset)
+  for (let i = 0; i < length; i++) {
+    let r = Math.random()
+    if (r > 0.666) {
+      output +='╳' 
+    }
+    else if (r <= 0.666 && r > 0.333){
+      output += '╲' 
+    }
+    else{
+      output += '╱' 
     }
   }
   console.log(output)
+
+  if (Math.random() > 0.5) {
+        offset += 1
+        if (offset > w - length){
+          offset = w - length
+        }
+        if (length > 2 && Math.random() > 0.5) {
+          length -= 1
+        }
+  }
+  else{
+    offset -= 1
+    if (offset < 0){
+      offset = 0
+    }
+    if (length < w - 1 - offset && Math.random() > 0.5) {
+        length += 1
+      }
+  }
 }
 
 draw()
